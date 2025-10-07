f21d415397ecf53fe846c335140feb65fee228eb
Yashvi Tank <yashvi-ghanshyam.tank@epita.fr>
2025-10-07 16:25:47 +0200

    Lake section initiated

--- src/components/HeroComp.tsx
+++ src/components/HeroComp.tsx
@@ -1,11 +1,18 @@
 import styles from "./HeroComp.module.scss";
+import bg from "../assets/bg.png";
+import lake from "../assets/02.png"; 
 
 export default function HeroComp() {
   return (
     <section className={styles.scene}>
-      {/* This is the “moon” div the guide starts with */}
-      <div className={styles.moon}></div>
-      {/* background, lake, house, title, trees, mountains, etc. */}
+      {/* background mountains */}
+      <img src={bg} alt="background mountains" className={styles.bg} />
+
+      {/* lake */}
+      <img src={lake} alt="lake of the mountain" className={styles.lake} />
+
+      {/* moon (kept with glow) */}
+      <div className={styles.moon} />
     </section>
   );
 }
