66b539ffc6c97f84bcd9217abc5a6afadd0b0492
Yashvi Tank <yashvi-ghanshyam.tank@epita.fr>
2025-10-11 13:20:07 +0200

    Fixed the title

--- src/components/HeroComp.tsx
+++ src/components/HeroComp.tsx
@@ -2,47 +2,25 @@ import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import styles from "./HeroComp.module.scss";
 
-import bgUrl from "../assets/bg.png";
-import lakeUrl from "../assets/02.png";
-import houseUrl from "../assets/04.png";
+import bg from "../assets/bg.png";
+import lake from "../assets/02.png";
+import house from "../assets/04.png";
 
 export default function HeroComp() {
   const ref = useRef<HTMLElement | null>(null);
 
-  
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["start start", "end start"], 
+    offset: ["start start", "end start"],
   });
-
- 
   const yLake = useTransform(scrollYProgress, [0, 1], ["0vh", "-14vh"]);
 
   return (
     <section ref={ref} className={styles.scene}>
-      
-      <div
-        className={`${styles.layer} ${styles.bg}`}
-        style={{ backgroundImage: `url(${bgUrl})` }}
-        aria-hidden
-      />
-
-      
-      <div
-        className={`${styles.layer} ${styles.house}`}
-        style={{ backgroundImage: `url(${houseUrl})` }}
-        role="img"
-        aria-label="House on the lake"
-      />
-
-      
-      <motion.div
-        className={`${styles.layer} ${styles.lake}`}
-        style={{ y: yLake, backgroundImage: `url(${lakeUrl})` }}
-        aria-hidden
-      />
-
-      
+      <img src={bg} alt="mountains" className={`${styles.layer} ${styles.bg}`} />
+      <img src={house} alt="house" className={`${styles.layer} ${styles.house}`} />
+      <motion.img src={lake} alt="lake" className={`${styles.layer} ${styles.lake}`} style={{ y: yLake }} />
+      <h1 className={styles.title}>Medieval Sharpness</h1>
       <div className={styles.moon} />
     </section>
   );
