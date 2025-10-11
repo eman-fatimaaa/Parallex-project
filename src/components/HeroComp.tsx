92467ddec0a7c633e220677cd6a866ccd62c5814
Yashvi Tank <yashvi-ghanshyam.tank@epita.fr>
2025-10-11 13:39:33 +0200

    Added the tree

--- src/components/HeroComp.tsx
+++ src/components/HeroComp.tsx
@@ -5,6 +5,7 @@ import styles from "./HeroComp.module.scss";
 import bg from "../assets/bg.png";
 import lake from "../assets/02.png";
 import house from "../assets/04.png";
+import tree from "../assets/06.png";
 
 export default function HeroComp() {
   const ref = useRef<HTMLElement | null>(null);
@@ -20,6 +21,7 @@ export default function HeroComp() {
       <img src={bg} alt="mountains" className={`${styles.layer} ${styles.bg}`} />
       <img src={house} alt="house" className={`${styles.layer} ${styles.house}`} />
       <motion.img src={lake} alt="lake" className={`${styles.layer} ${styles.lake}`} style={{ y: yLake }} />
+      <img src={tree} alt="tree branch" className={`${styles.layer} ${styles.tree}`} />
       <h1 className={styles.title}>Medieval Sharpness</h1>
       <div className={styles.moon} />
     </section>
